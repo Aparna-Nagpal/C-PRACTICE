998dc7535edfd3c594dabd178382929237391ae9
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-27 21:36:35 +0530

    Create 6.C

---
+++ CODING/6.C
@@ -0,0 +1,10 @@
+//Program to copy one string into another
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+   char t[40]="aparna",y[40];
+   strcpy(y,t);
+   printf("%s",y);
+   return 0;
+}
