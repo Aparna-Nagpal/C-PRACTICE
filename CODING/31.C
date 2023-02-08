c2037e852c3c87bdfd5fe7e464d14eaca065ab48
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-02-08 22:59:41 +0530

    Create 31.C

---
+++ CODING/31.C
@@ -0,0 +1,29 @@
+/*
+BATTERY HEALTH
+Apple considers any iPhone with a battery health of 80% or above, to be in optimal condition.
+Given that your iPhone has X% battery health, find whether it is in optimal condition.
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+The first and only line of each test case contains an integer X — the battery health.
+Output Format
+For each test case, output on a new line, YES, if the battery is in optimal condition, and NO otherwise.
+You may print each character in uppercase or lowercase. For example, NO, no, No and nO, are all considered identical.
+*/
+#include <stdio.h>
+
+int main(void) {
+    int t,a;
+    scanf("%d",&t);
+    while(t>0)
+    {
+        t--;
+        scanf("%d",&a);
+        if(a>=80)
+        printf("Yes\n");
+        else
+        printf("no\n");
+    }
+	// your code goes here
+	return 0;
+}
+
