b0a36064ebab8e0c72356caae4ffd52deaf2cf07
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-02-16 19:10:08 +0530

    Create 12.C

---
+++ C++ PRACTICE/12.C
@@ -0,0 +1,29 @@
+/*
+TOM AND JERRY CHASE difficulty rating:298
+In a classic chase, Tom is running after Jerry as Jerry has eaten Tom's favourite food.
+Jerry is running at a speed of X metres per second while Tom is chasing him at a speed of Y metres per second. Determine whether Tom will be able to catch Jerry.
+Note that initially Jerry is not at the same position as Tom.
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of two space-separated integers X and Y — the speeds of Jerry and Tom respectively.
+Output Format
+For each test case, output on a new line, YES, if Tom will be able to catch Jerry. Otherwise, output NO.
+You can print each character in uppercase or lowercase. For example NO, no, No, and nO are all considered the same.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t>0)
+	{
+	    t--;
+	    cin>>x>>y;
+	    if(y>x)
+	    cout<<"yes"<<endl;
+	    else
+	    cout<<"no"<<endl;
+	}
+	return 0;
+}
