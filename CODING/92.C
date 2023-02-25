9878f0f9a8195c531b9aa1a2fed170a49209a627
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-02-25 23:47:34 +0530

    Create 92.C

---
+++ CODING/92.C
@@ -0,0 +1,22 @@
+/*
+THE HIDDEN NUMBERS difficulty rating:917
+Salmon has some hidden numbers that he wants you to find!
+Given an integer N, find and output any two integers A and B such that:1≤A,B≤10  , and A∗B=N.
+###Input: The first line of input consists of a single integer 1≤T≤10 - the number of testcases.
+The next T lines will consist of one integer, 1≤ N≤10 .
+###Output: For each of the T testcases, output two space-separated integers A and B.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,a;
+	cin>>t;
+	while(t>0)
+	{
+	    t--;
+	    cin>>a;
+	    cout<<"1 "<<a<<endl;
+}
+	return 0;
+}
