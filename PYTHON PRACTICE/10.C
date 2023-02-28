1729918b52e458c44b4b0b88a93884b3446af10b
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-02-28 17:20:31 +0530

    Create 10.C

---
+++ PYTHON PRACTICE/10.C
@@ -0,0 +1,20 @@
+'''
+FLIP THE CARDS
+There are N cards on a table, out of which X cards are face-up and the remaining are face-down.
+In one operation, we can do the following:
+Select any one card and flip it (i.e. if it was initially face-up, after the operation, it will be face-down and vice versa)
+What is the minimum number of operations we must perform so that all the cards face in the same direction (i.e. either all are face-up or all are face-down)?
+Input Format
+The first line contains a single integer T — the number of test cases. Then the test cases follow.
+The first and only line of each test case contains two space-separated integers N and X — the total number of cards and the number of cards which are initially face-up.
+Output Format
+For each test case, output the minimum number of cards you must flip so that all the cards face in the same direction.
+  '''
+  t=int(input())
+while(t>0):
+    t=t-1
+    a,b=map(int,input().split())
+    if(b<=a-b):
+        print(b)
+    else:
+        print(a-b)
