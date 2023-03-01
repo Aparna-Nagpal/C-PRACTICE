5651eef43502b2f9e7cc92be721dcaf4ca0157e0
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-03-01 18:38:55 +0530

    Create 15.C

---
+++ PYTHON PRACTICE/15.C
@@ -0,0 +1,31 @@
+'''
+  WATER MIXING difficulty rating:694
+  Chef is setting up a perfect bath for himself. He has X litres of hot water and Y litres of cold water.
+The initial temperature of water in his bathtub is A degrees. On mixing water, the temperature of the bathtub changes as following:
+The temperature rises by 1 degree on mixing 1 litre of hot water.
+The temperature drops by 1 degree on mixing 1 litre of cold water.
+Determine whether he can set the temperature to B degrees for a perfect bath.
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of four space-separated integers A,B,X, and Y — the initial temperature of bathtub, the desired temperature of bathtub, the amount of hot water in litres, and the amount of cold water in litres respectively.
+Output Format
+For each test case, output on a new line, YES if Chef can get the desired temperature for his bath, and NO otherwise.
+You may print each character of the string in uppercase or lowercase (for example, the strings YES, yEs, yes, and yeS will all be treated as identical).
+  '''
+  t=int(input())
+while(t>0):
+    t=t-1
+    a,b,c,d=map(int,input().split())
+    s=b-a
+    if(s>0):
+        if(c>=s):
+            print("yes")
+        else:
+            print("no")
+    else:
+        s=s*-1
+        if(d>=s):
+            print("yes")
+        else:
+            print("no")
+            
